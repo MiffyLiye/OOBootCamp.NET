1defcd6015c2c06f4f5cc22730d7585f619e471c
WANG Tao <twang@thoughtworks.com>
2016-09-06 18:56:53 +0800

    Add ParkingLot and Test projects

---
+++ src/ParkingLot/ParkingLot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOBootCamp
+{
+    public class Car
+    {
+    }
+
+    public class ParkingLot
+    {
+        private readonly Dictionary<string, Car> _lot = new Dictionary<string, Car>();
+
+        public string Park(Car car)
+        {
+            var token = Guid.NewGuid().ToString();
+            _lot.Add(token, car);
+            return token;
+        }
+
+        public Car Pick(string token)
+        {
+            if (!_lot.ContainsKey(token))
+            {
+                throw new InvalidOperationException("Not Found");
+            }
+            var car = _lot[token];
+            _lot.Remove(token);
+            return car;
+        }
+    }
+}
---
+++ test/ParkingLotTest/ParkingLotTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using FluentAssertions;
+using OOBootCamp;
+using Xunit;
+
+namespace OOBootCampTest
+{
+    public class ParkingLotTest
+    {
+        [Fact]
+        public void should_pick_the_same_car_after_parked_car()
+        {
+            var parkingLot = new ParkingLot();
+            var car = new Car();
+
+            var token = parkingLot.Park(car);
+            var pickedCar = parkingLot.Pick(token);
+
+            pickedCar.Should().BeSameAs(car);
+        }
+
+        [Fact]
+        public void should_pick_the_corresponding_car_after_parked_two_cars()
+        {
+            var parkingLot = new ParkingLot();
+            var myCar = new Car();
+            var myToken = parkingLot.Park(myCar);
+            var otherCar = new Car();
+            var otherToken = parkingLot.Park(otherCar);
+
+            var pickedMyCar = parkingLot.Pick(myToken);
+            var pickedOtherCar = parkingLot.Pick(otherToken);
+
+            pickedMyCar.Should().BeSameAs(myCar);
+            pickedOtherCar.Should().BeSameAs(otherCar);
+        }
+
+        [Fact]
+        public void should_not_pick_car_successfully_when_car_already_picked()
+        {
+            var parkingLot = new ParkingLot();
+            var car = new Car();
+            var token = parkingLot.Park(car);
+            parkingLot.Pick(token);
+
+            parkingLot.Invoking(p => p.Pick(token))
+                .ShouldThrow<InvalidOperationException>()
+                .WithMessage("Not Found");
+        }
+    }
+}
