3c5bb46c346abe9a8300e31e3920485f10233101
WANG Tao <twang@thoughtworks.com>
2016-09-09 21:42:21 +0800

    move and split tests

---
+++ test/ParkingLotTest/ParkingLot/ParkingLotStatusTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FluentAssertions;
+using OOBootCamp;
+using Xunit;
+
+namespace OOBootCampTest.UT
+{
+    public class ParkingLotStatusTests
+    {
+        [Fact]
+        public void should_get_can_park_info_when_parking_lot_is_not_full()
+        {
+            var parkingLot = new ParkingLot(1);
+
+            parkingLot.CanPark().Should().BeTrue();
+        }
+
+        [Fact]
+        public void should_get_cannot_park_info_when_parking_lot_is_full()
+        {
+            var parkingLot = new ParkingLot(1);
+            parkingLot.Park(new Car());
+
+            parkingLot.CanPark().Should().BeFalse();
+        }
+    }
+}
--- test/ParkingLotTest/ParkingLot/ParkingLotTest.cs
+++ test/ParkingLotTest/ParkingLot/ParkingLotTest.cs
@@ -69,22 +69,5 @@ namespace OOBootCampTest
                 .ShouldThrow<InvalidOperationException>()
                 .WithMessage("Not found.");
         }
-
-        [Fact]
-        public void should_get_can_park_info_when_parking_lot_is_not_full()
-        {
-            var parkingLot = new ParkingLot(1);
-
-            parkingLot.CanPark().Should().BeTrue();
-        }
-
-        [Fact]
-        public void should_get_cannot_park_info_when_parking_lot_is_full()
-        {
-            var parkingLot = new ParkingLot(1);
-            parkingLot.Park(new Car());
-
-            parkingLot.CanPark().Should().BeFalse();
-        }
     }
 }
